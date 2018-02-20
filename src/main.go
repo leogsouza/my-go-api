ac70b8fa009be01911610cf7763b4b7bef9543de
Leonardo Souza <leogsouza@gmail.com>
2018-02-20 19:32:40 +0000

    Handling parameters

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -12,11 +13,27 @@ import (
 func main() {
 	var router = mux.NewRouter()
 	router.HandleFunc("/healthcheck", healthCheck).Methods("GET")
+	router.HandleFunc("/message", handleQryMessage).Methods("GET")
+	router.HandleFunc("/m{msg}", handleUrlMesssage).Methods("GET")
 
 	fmt.Println("Running server!")
-	log.Fatal(http.ListenAndServe(":3000", router))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Still Alive!")
+}
+
+func handleQryMessage(w http.ResponseWriter, r *http.Request) {
+	vars := r.URL.Query()
+	message := vars.Get("msg")
+
+	json.NewEncoder(w).Encode(map[string]string{"message": message})
+}
+
+func handleUrlMessage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	message := vars["msg"]
+
+	json.NewEncoder(w).Encode(map[string]string{"message": message})
 }
